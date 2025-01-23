c262dc6891b6734f686e21d974484abb924f29aa
mantasg6 <mantasgricius6@gmail.com>
2025-01-22 19:32:55 -0500

    add unit tests
    add unit tests and break code to check if CI catches it

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+	"reflect"
+)
+
+func TestNoAuth(t *testing.T) {
+	header := http.Header{}
+	_, got := GetAPIKey(header)
+	want := errors.New("no authorization header included")
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+   }
+}
+
+func TestAuthFormat(t *testing.T) {
+	header := http.Header{
+		"Authorization": {"login"},
+	}
+	_, got := GetAPIKey(header)
+	want := errors.New("malformed authorization header")
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+   }
+}
+
+func TestSuccess(t *testing.T) {
+	header := http.Header{
+		"Authorization": {"ApiKey @jnnasdo1.,3214naJ!@HBFJS@IJIU$#@NUIANDSfn23"},
+	}
+	got, _ := GetAPIKey(header)
+	want := "@jnnasdo1.,3214naJ!@HBFJS@IJIU$#@NUIANDSfn23"
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+   }
+}
